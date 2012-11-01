ddda10ea96a0efcb5fe3ea40784a5c086414e279
rmonico <rmonico1@gmail.com>
2012-11-01 14:04:21 -0200

    Criada nova atividade para tela de boas vindas, arquivo de instruções adicionado, criação do fluxograma de atividades

--- src/br/zero/droidcam/MainActivity.java
+++ src/br/zero/droidcam/MainActivity.java
@@ -1,88 +1,30 @@
 package br.zero.droidcam;
 
 import android.app.Activity;
-import android.hardware.Camera;
-import android.hardware.Camera.CameraInfo;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-	private static final String DEBUG_TAG = "MainActivity";
-	private Camera mCamera;
-	private CameraPreview mCameraSurfaceView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		int frontCameraId = findFrontFacingCamera();
-
-		// Create an instance of Camera
-		mCamera = getCameraInstance(frontCameraId);
-		
-		mCamera.setDisplayOrientation(90);
-
-		if (mCamera == null) {
-			Toast.makeText(null, "Camera not found", Toast.LENGTH_SHORT).show();
-			return;
-		}
-
-		// Create our Preview view and set it as the content of our activity.
-		mCameraSurfaceView = new CameraPreview(this, mCamera);
-
-		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-		preview.addView(mCameraSurfaceView);
-	}
-
-	private int findFrontFacingCamera() {
-		int cameraId = -1;
-		// Search for the front facing camera
-		int numberOfCameras = Camera.getNumberOfCameras();
-		for (int i = 0; i < numberOfCameras; i++) {
-			CameraInfo info = new CameraInfo();
-			Camera.getCameraInfo(i, info);
-			if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
-				Log.d(DEBUG_TAG, "Camera found");
-				cameraId = i;
-				break;
-			}
-		}
-		return cameraId;
-	}
-
-	/** A safe way to get an instance of the Camera object. */
-	public Camera getCameraInstance(int cameraId) {
-		Camera c = null;
-		try {
-			c = Camera.open(cameraId); // attempt to get a Camera instance
-		} catch (Exception e) {
-			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-		}
-		return c; // returns null if camera is unavailable
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	@Override
-	protected void onPause() {
-		if (mCamera != null) {
-			mCamera.release();
-			mCamera = null;
-		}
-		super.onPause();
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void continuarOnClick(View view) {
+    	Intent intent = new Intent(this, CameraActivity.class);
+    	
+    	intent.putExtra("name", "alguma coisa");
+    	
+    	startActivity(intent);
+    }
 }
