ec6f3bb86285c744c875fdb9801490272e74cf39
rmonico <rmonico1@gmail.com>
2012-11-07 13:38:36 -0200

    Passando o nome para a tela home

--- src/br/zero/euportinari/HomeActivity.java
+++ src/br/zero/euportinari/HomeActivity.java
@@ -3,6 +3,7 @@ package br.zero.euportinari;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 
@@ -10,9 +11,19 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        TextView name = (TextView) findViewById(R.id.TextView1);
+        
+        String selectedName = getSelectedName();
+        
+        name.setText(selectedName);
     }
 
-    @Override
+    private String getSelectedName() {
+		return (String) getIntent().getExtras().get("name");
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
--- src/br/zero/euportinari/NameSelectActivity.java
+++ src/br/zero/euportinari/NameSelectActivity.java
@@ -5,24 +5,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class NameSelectActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_name_select);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_name_select);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_name_select, menu);
-        return true;
-    }
-    
-    public void selectNameOnClick(View view) {
-    	Intent intent = new Intent(this, HomeActivity.class);
-    	
-    	startActivity(intent);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_name_select, menu);
+		return true;
+	}
+
+	public void selectNameOnClick(View view) {
+		Intent intent = new Intent(this, HomeActivity.class);
+
+		intent.putExtra("name", getName());
+
+		startActivity(intent);
+	}
+
+	private String getName() {
+		EditText name = (EditText) findViewById(R.id.editTextName);
+		
+		return name.getText().toString();
+	}
 }
