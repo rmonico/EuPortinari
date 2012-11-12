c05a3978524fe404114b4546b5de072c6378aaf6
rmonico <rmonico1@gmail.com>
2012-11-12 08:19:00 -0200

    Novos imagens

--- src/br/zero/euportinari/HomeActivity.java
+++ src/br/zero/euportinari/HomeActivity.java
@@ -1,9 +1,10 @@
 package br.zero.euportinari;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.TextView;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -12,11 +13,11 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         
-        TextView name = (TextView) findViewById(R.id.TextView1);
-        
-        String selectedName = getSelectedName();
-        
-        name.setText(selectedName);
+//        TextView name = (TextView) findViewById(R.id.TextView1);
+//        
+//        String selectedName = getSelectedName();
+//        
+//        name.setText(selectedName);
     }
 
     private String getSelectedName() {
@@ -28,4 +29,10 @@ public class HomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
     }
+	
+	public void onPiaoClick(View view) {
+		Intent intent = new Intent(this, CameraActivity.class);
+		
+		startActivity(intent);
+	}
 }
