8a17a3135cc60fcde4355a503d3468a80fac09e3
rmonico <rmonico1@gmail.com>
2012-11-13 20:34:11 -0200

    Primeira versão da funcionalidade de salvar a foto

--- src/br/zero/euportinari/CameraActivity.java
+++ src/br/zero/euportinari/CameraActivity.java
@@ -1,18 +1,27 @@
 package br.zero.euportinari;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements PictureCallback {
 
-	private static final String DEBUG_TAG = "MainActivity";
+	private static final String TAG = "MainActivity";
 	private Camera mCamera;
 	private CameraSurface mCameraSurfaceView;
 
@@ -22,15 +31,17 @@ public class CameraActivity extends Activity {
 		setContentView(R.layout.activity_camera);
 
 		int frontCameraId = findFrontFacingCamera();
-		
+
 		if (frontCameraId == -1) {
-			Toast.makeText(this, "Câmera frontal não encontrada, usando camera padrão.", Toast.LENGTH_LONG);
+			Toast.makeText(this,
+					"Câmera frontal não encontrada, usando camera padrão.",
+					Toast.LENGTH_LONG);
 			frontCameraId = 0;
 		}
-		
+
 		// Create an instance of Camera
 		mCamera = getCameraInstance(frontCameraId);
-		
+
 		if (mCamera == null) {
 			Toast.makeText(this, "Camera not found", Toast.LENGTH_SHORT).show();
 			return;
@@ -53,7 +64,7 @@ public class CameraActivity extends Activity {
 			CameraInfo info = new CameraInfo();
 			Camera.getCameraInfo(i, info);
 			if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
-				Log.d(DEBUG_TAG, "Camera found");
+				Log.d(TAG, "Camera found");
 				cameraId = i;
 				break;
 			}
@@ -67,7 +78,8 @@ public class CameraActivity extends Activity {
 		try {
 			c = Camera.open(cameraId); // attempt to get a Camera instance
 		} catch (Exception e) {
-			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(), e.getMessage(),
+					Toast.LENGTH_LONG).show();
 		}
 		return c; // returns null if camera is unavailable
 	}
@@ -91,8 +103,69 @@ public class CameraActivity extends Activity {
 		}
 		super.onPause();
 	}
-	
+
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+
+		File pictureFile = getOutputMediaFile();
+		if (pictureFile == null) {
+			Log.d(TAG, "Error creating media file, check storage permissions.");
+			return;
+		}
+
+		try {
+			Toast.makeText(this, "onTakePicture", Toast.LENGTH_LONG).show();
+			FileOutputStream fos = new FileOutputStream(pictureFile);
+			fos.write(data);
+			fos.close();
+
+			Toast.makeText(CameraActivity.this,
+					"Imagem salva em: " + pictureFile.getName(),
+					Toast.LENGTH_LONG).show();
+
+		} catch (FileNotFoundException e) {
+			Log.d(TAG, "File not found: " + e.getMessage());
+		} catch (IOException e) {
+			Log.d(TAG, "Error accessing file: " + e.getMessage());
+		}
+	}
+
+	private static File getOutputMediaFile() {
+
+		// File mediaStorageDir = new File(
+		// Environment.getExternalStoragePublicDirectory(
+		//
+		// Environment.DIRECTORY_PICTURES), TAG);
+		//
+		// if (!mediaStorageDir.exists()) {
+		//
+		// if (!mediaStorageDir.mkdirs()) {
+		//
+		// Log.d(TAG, "failed to create directory");
+		//
+		// return null;
+		//
+		// }
+		//
+		// }
+		//
+		// // Create a media file name
+		//
+		// String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+		// .format(new Date());
+		//
+		// File mediaFile;
+		//
+		// mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+		//
+		// "IMG_" + timeStamp + ".jpg");
+		//
+		// return mediaFile;
+		//
+		return new File("/sdcard/external_sd/files/image.jpg");
+	}
+
 	public void onTakePicture(View view) {
-		Toast.makeText(this, "onTakePicture", Toast.LENGTH_LONG).show();
+		mCamera.takePicture(null, null, this);
 	}
 }
