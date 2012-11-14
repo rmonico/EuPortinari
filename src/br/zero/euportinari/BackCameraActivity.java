4869af396193fc45f83ce91378ca0a9641aef3e1
Rafael Monico <rmonico@hacklab.com.br>
2012-11-14 01:03:54 -0200

    Fazendo o preview para a atividade de camera traseira

--- src/br/zero/euportinari/BackCameraActivity.java
+++ src/br/zero/euportinari/BackCameraActivity.java
@@ -1,18 +1,79 @@
 package br.zero.euportinari;
 
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class BackCameraActivity extends Activity {
 
-    @Override
+    private static final String TAG = "BackCameraActivity";
+	private Camera mCamera;
+	private CameraSurface mCameraSurfaceView;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_camera);
+
+        int backCameraId = findBackFacingCamera();
+
+		if (backCameraId == -1) {
+			Toast.makeText(this,
+					"Câmera traseira não encontrada, usando camera padrão.",
+					Toast.LENGTH_LONG);
+			backCameraId = 0;
+		}
+
+		// Create an instance of Camera
+		mCamera = getCameraInstance(backCameraId);
+
+		if (mCamera == null) {
+			Toast.makeText(this, "Camera not found", Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		mCamera.setDisplayOrientation(90);
+
+		// Create our Preview view and set it as the content of our activity.
+		mCameraSurfaceView = new CameraSurface(this, mCamera);
+
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mCameraSurfaceView);
     }
 
-    @Override
+	public Camera getCameraInstance(int cameraId) {
+		Camera c = null;
+		try {
+			c = Camera.open(cameraId); // attempt to get a Camera instance
+		} catch (Exception e) {
+			Toast.makeText(getApplicationContext(), e.getMessage(),
+					Toast.LENGTH_LONG).show();
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	private int findBackFacingCamera() {
+		int cameraId = -1;
+		// Search for the front facing camera
+		int numberOfCameras = Camera.getNumberOfCameras();
+		for (int i = 0; i < numberOfCameras; i++) {
+			CameraInfo info = new CameraInfo();
+			Camera.getCameraInfo(i, info);
+			if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+				Log.d(TAG, "Camera found");
+				cameraId = i;
+				break;
+			}
+		}
+		return cameraId;
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_back_camera, menu);
         return true;
